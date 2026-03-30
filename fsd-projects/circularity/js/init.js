c4def6ecfadf910c7420becaee9c1a4c5c9e275a
KirstynCooper7 <kirstyn.cooper@jeffersonrise.org>
2026-03-30 17:00:48 +0000

    message

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,17 +20,23 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
 
         // TODO 2 : Create a function that draws a circle 
-        
-
-
+       function drawCircle() {
+        circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+        physikz.addRandomVelocity(circle, canvas, 5, 5);
+        view.addChild(circle);
+        circles.push(circle);
+}
         // TODO 3 : Call the drawCircle() function
-
-
-
+          drawCircle();
+          drawCircle();
+          drawCircle();
+          drawCircle();
+          drawCircle();
         // TODO 7 : Use a loop to create multiple circles
 
 
@@ -47,11 +53,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-
-            
+        
+            physikz.updatePosition(circles[0]);
+        physikz.updatePosition(circles[1]);
+        physikz.updatePosition(circles[2]);
+        physikz.updatePosition(circles[3]);
+        physikz.updatePosition(circles[4]);
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
-
+        //  game.checkCirclePosition(circles[0]);
+        //  game.checkCirclePosition(circles[1]);
+        //  game.checkCirclePosition(circles[2]);
+        //  game.checkCirclePosition(circles[3]);
+        //  game.checkCirclePosition(circles[4]);
             // TODO 8 / TODO 9 : Iterate over the array
            
             
@@ -65,16 +78,11 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
-            }
+          
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
-
+           
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
-        }
         
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
@@ -97,4 +105,5 @@ if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
+ }
 }
